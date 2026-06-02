34347d8db160f0f909861fa2ddad44e0fbdf6a46
NeilMathow <neilmathowmsn@gmail.com>
2026-06-02 15:54:46 +0000

    fix FundedNext logo sizing

--- app/page.js
+++ app/page.js
@@ -129,10 +129,11 @@ const FIRMS = [
   )},
   { name: "FundedNext", connected: false, comingSoon: true, color: "#6366f1", logo: (
     <div style={{ width: "42px", height: "42px", borderRadius: "10px", background: "#000", display: "flex", alignItems: "center", justifyContent: "center", overflow: "hidden" }}>
-      <svg width="36" height="36" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M10 20 L10 80 L25 80 L25 55 L45 55 L45 42 L25 42 L25 33 L48 33 L48 20 Z" fill="white"/>
-        <path d="M52 20 L52 80 L67 80 L67 42 L85 20 Z" fill="white"/>
-        <polygon points="67,20 85,20 85,38" fill="#6366f1"/>
+      <svg width="42" height="42" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100" height="100" fill="#000"/>
+        <path d="M8 15 L8 85 L22 85 L22 57 L42 57 L42 44 L22 44 L22 28 L45 28 L45 15 Z" fill="white"/>
+        <path d="M50 15 L50 85 L64 85 L64 38 L84 15 Z" fill="white"/>
+        <polygon points="64,15 84,15 84,35" fill="#6366f1"/>
       </svg>
     </div>
   )},
